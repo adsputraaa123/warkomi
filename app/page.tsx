762255d36074ffa9bbfd9d11a67817edc76bae43
Adisaputra Nur Arminta <adsputraaa123@gmail.com>
2025-10-23 14:10:35 +0700

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default async function Home() {
-  return <div>Warkomi Home</div>;
+  return (
+    <div>
+      <Link href={"/dashboard?id=1"}>Dashboard</Link>
+    </div>
+  );
 }
